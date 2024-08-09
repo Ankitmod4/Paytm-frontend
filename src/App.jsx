9340061a775f57fb65db815c51eb116b87a7dd79
Ankit Mod <ankit300mod@gmail.com>
2024-08-09 14:10:36 +0530

    data pushed

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { BrowserRouter, Route,Routes } from 'react-router-dom'
+import {SignupPage} from './pages/SignupPage'
+import {SigninPage} from './pages/SigninPage'
+import {DashboardPage} from './pages/DashboardPage'
+import {SendMoneyPage} from './pages/SendMoneyPage'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter>
+
+      <Routes>
+        <Route path='/signup' element={<SignupPage></SignupPage>}/>
+        <Route path='/signin' element={<SigninPage></SigninPage>}/>
+        <Route path='/dashboard' element={<DashboardPage></DashboardPage>}/>
+        <Route path='/send' element={<SendMoneyPage></SendMoneyPage>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
